6581e8cfe79948fba453b4095db558703d31d2c9
DanieleDiSpirito <danieledispirito2004@gmail.com>
2022-06-30 15:29:02 +0200

    più caronti

---
+++ caronte_fixme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#	.		TIMEOUT    CARONTE_IP
+#./caronte.sh 30 caronte.com game
+
+if [[ "$#" -ne 3 ]]; then
+	echo "Usage: ./caronte.sh <timeout> <ip:port> <interface>"
+	exit 2
+fi
+
+TIMEOUT_TCPDUMP="$1"
+CARONTE_ADDR="$2"
+INTERFACE_NAME="$3"
+
+while true; do
+	timeout $TIMEOUT_TCPDUMP tcpdump -w data.pcap -i $INTERFACE_NAME port 8080
+	curl -F "file=@data.pcap" -F "flush_all=true" "http://$CARONTE_ADDR/api/pcap/upload"
+	rm data.pcap
+done
