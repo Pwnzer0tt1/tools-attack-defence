55b5fd349a2128ca11a49beb939da852ca62025a
linoe97 <nicola.pace97@gmail.com>
2022-06-30 02:15:55 +0200

    added gitbackup caronte.sh

---
+++ caronte.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#	.		TIMEOUT    CARONTE_IP
+#./caronte.sh 30 caronte.com game
+
+if [[ "$#" -ne 3 ]]; then
+	echo "Usage: ./caronte.sh <timeout> <ip:port> <interface>"
+	exit 2
+fi
+
+TIMEOUT_TCPDUMP="$1"
+CARONTE_ADDR="$2"
+INTERFACE_NAME="$3"
+
+while true; do
+	timeout $TIMEOUT_TCPDUMP tcpdump -w data.pcap -i $INTERFACE_NAME port not 22 and port not 4444
+	curl -F "file=@data.pcap" -F "flush_all=true" "http://$CARONTE_ADDR/api/pcap/upload"
+	rm data.pcap
+done
